2bdaeb8f465c2ab8a044c570b6c7636be1cb3f45
huaweimin <huaweimin@yeah.net>
2021-01-20 17:36:37 +0800

    first commit

---
+++ travel/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <same-header></same-header>
+    <search></search>
+  </div>
+</template>
+
+<script>
+import SameHeader from '../components/SameHeader.vue'
+import Search from '../components/Search.vue'
+
+export default {
+  components: {
+    SameHeader,
+    Search
+  }
+}
+</script>
